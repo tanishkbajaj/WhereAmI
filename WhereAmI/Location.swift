561b4e5e980120810659fa3fbe66a32f8e87af25
Sujeet Poudel <sujeetpoudel.np@gmail.com>
2019-09-21 01:08:29 -0500

    Data Model with Date and Time for History Items

---
+++ WhereAmI/Location.swift
@@ -0,0 +1,26 @@
+//
+//  Location.swift
+//  WhereAmI
+//
+//  Created by IMCS on 9/20/19.
+//  Copyright © 2019 Tanishk. All rights reserved.
+//
+
+import Foundation
+struct Location {
+    var name : String = ""
+    var address : String = ""
+    var longitude : Double = 0.0
+    var latitude : Double = 0.0
+    var date : Date = Date()
+    init () {
+        
+    }
+    init( _ address : String, _ longitude : Double, _ latitude : Double, _ date : Date, _ name : String  = "") {
+        self.name = name
+        self.address = address
+        self.longitude = longitude
+        self.latitude = latitude
+        self.date = Date()
+    }
+}
